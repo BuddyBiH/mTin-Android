9233e46dee0e107762d6c5a81a2cad1599a0a950
BuddyBiH <nedim.korjenic@gmail.com>
2016-07-01 07:48:36 +0200

    Last commit

--- src/test/java/mTin/SmokeTest.java
+++ src/test/java/mTin/SmokeTest.java
@@ -2,52 +2,62 @@ package mTin;
 
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidKeyCode;
 import io.appium.java_client.remote.MobileCapabilityType;
 import io.appium.java_client.remote.MobilePlatform;
 
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
+
+import com.thoughtworks.selenium.webdriven.commands.KeyEvent;
 
 
 public class SmokeTest {
   @Test
   public void f() throws MalformedURLException, InterruptedException {
 	  
-	  File appDir = new File("src/test/resources");
-	  File app = new File (appDir,"app-debug.apk");
 	  
-	  DesiredCapabilities cap = new DesiredCapabilities();
+	    DesiredCapabilities cap = new DesiredCapabilities();
 	  
-	    cap.setCapability(MobileCapabilityType.APP, app);
+	    
 		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Emulator");
 		cap.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID);
 		cap.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT,360);
+		cap.setCapability(MobileCapabilityType.APP_PACKAGE, "com.demo.mtin.mtin");
+		cap.setCapability(MobileCapabilityType.APP_ACTIVITY, "com.demo.mtin.mtin.SplashActivity");
 		
 		AndroidDriver driver = new AndroidDriver(new URL("http://0.0.0.0:4723/wd/hub"),cap);
 		
-		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(120, TimeUnit.SECONDS);
 		
-		
+		//Waiting for elements to load
 		WebDriverWait wait = new WebDriverWait(driver,30);
 		WebElement el = driver.findElementById("com.demo.mtin.mtin:id/imageView");
 		wait.until(ExpectedConditions.elementToBeClickable(el));
 
-		driver.findElementByClassName("android.widget.ImageView").click();
+		List<WebElement> liste = driver.findElementsById("com.demo.mtin.mtin:id/imageView");
+		liste.get(0).click();
 		
-		driver.findElementById("com.demo.mtin.mtin:id/title").click();
+		String r = driver.findElementById("com.demo.mtin.mtin:id/review_naslov").getText();
 		
-		TouchAction touch = new TouchAction(driver);
-			
-		touch.tap(245, 269).perform();
-	  
-		TimeUnit.SECONDS.sleep(3);
+		Assert.assertEquals(r, "Reviews");
+		
+		driver.closeApp();
+		driver.quit();
+		
+	
   }
 }
